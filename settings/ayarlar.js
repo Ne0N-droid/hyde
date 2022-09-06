35ef35e489da37f6c5c07008023259d8cea396b3
Ne0N-droid <75836325+Ne0N-droid@users.noreply.github.com>
2022-09-06 22:18:46 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
-    PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    TOKEN: process.env.TOKEN || "ODg1NTMwNzczOTM4MTM5MTM2.GieVrU.EJEQHG65ahTnwdrh_t_7dMZRcWOhPYZiaNV_A0",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "/", // bot prefix
+    SAHİP: process.env.SAHİP || "469845300526317568", //sahip id :()
 }
